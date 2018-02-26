431952c926425b02c2d1d641b2f0f1b299bf1834
Adi Hayat <adi.hayat@mobileye.com>
2018-02-26 16:43:15 +0200

    update .vimrc

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -31,7 +31,7 @@ source ~/.vim_runtime/vimrcs/taglist.vim
 
 map <F4> :Tlist<CR>
 map <F2> :Explore<CR>
-nnoremap <F3> :! rm -f ctags cscope.out ; ctags --c++-kinds=+p --fields=+iaS --extra=+q -R . ; cscope -R -b<CR>
+nnoremap <F3> :! rm -f ctags cscope.out ; ctags --c++-kinds=+p --fields=+iaS --extra=+q -R . &  cscope -R -b &<CR>
 set nu
 set mouse=a
 nnoremap <C-c> :shell<CR>
@@ -42,9 +42,10 @@ nnoremap <C-P> :Unite -start-insert file_rec/async<cr>
 nnoremap <F5> :Unite -force-redraw -start-insert file_rec/async<cr>
 nnoremap <F6> :setlocal spell!<cr>
 nnoremap <F8> :MRU<cr>
+nnoremap <F9> :YcmCompleter GetDoc<cr>
 " nnoremap <space>/ :Unite grep:.<cr>
 nnoremap <space>g :Unite grep/git:.:<cr>
-nnoremap <C-?> :Unite -start-insert tag<cr>
+nnoremap <F7> :Unite -start-insert tag<cr>
 nnoremap <space>/ :Ag  
 cmap Q bufdo q
 let g:unite_source_history_yank_enable = 1
@@ -62,6 +63,8 @@ set softtabstop=0 noexpandtab
 set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
 "" Change to Project specific to add project included path 
 let g:ycm_global_ycm_extra_conf = "/usr/lib/vim-youcompleteme/ycm_extra_conf.py"
+let g:ycm_auto_trigger = 0
+let g:ctrlp_max_files=0
 set foldmethod=syntax
 set foldlevel=999
 set foldclose=all
